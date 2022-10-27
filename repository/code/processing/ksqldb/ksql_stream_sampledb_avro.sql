785535512e335c19f3854c362b8b1fe5572bf66a
dataeng <viniciusdvale@gmail.com>
2022-10-27 17:26:41 -0300

    label cluster manifest

--- repository/code/processing/ksqldb/ksql_stream_sampledb_avro.sql
+++ repository/code/processing/ksqldb/ksql_stream_sampledb_avro.sql
@@ -4,3 +4,14 @@
 SET 'auto.offset.reset' = 'earliest';
 SHOW TOPICS;
 CREATE OR REPLACE STREAM ksql_stream_sampledb_customer_avro WITH (KAFKA_TOPIC='src-sqlserver-jdbc-sampledb-custumer-avro', VALUE_FORMAT='AVRO', KEY_FORMAT='AVRO');
+CREATE OR REPLACE STREAM ksql_stream_sampledb_salesorderheader_avro WITH (KAFKA_TOPIC='src-sqlserver-jdbc-sampledb-salesorderheader-avro', VALUE_FORMAT='AVRO', KEY_FORMAT='AVRO');
+select * from ksql_stream_sampledb_customer_avro emit changes limit 100;
+select * from ksql_stream_sampledb_salesorderheader_avro emit changes limit 10;
+SELECT SalesPerson, count(SalesPerson) as "number_clients" from ksql_stream_sampledb_customer_avro WINDOW SESSION (5 SECONDS) group by SalesPerson EMIT CHANGES;
+
+
+
+
+
+
+
