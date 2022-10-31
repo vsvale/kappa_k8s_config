9068f92c019b2c01fa576dcfccdac630d5f74ff1
dataeng <viniciusdvale@gmail.com>
2022-10-31 09:35:08 -0300

    manifest cert-manager

--- repository/code/processing/ksqldb/ksql_stream_sampledb_avro.sql
+++ repository/code/processing/ksqldb/ksql_stream_sampledb_avro.sql
@@ -5,10 +5,28 @@ SET 'auto.offset.reset' = 'earliest';
 SHOW TOPICS;
 CREATE OR REPLACE STREAM ksql_stream_sampledb_customer_avro WITH (KAFKA_TOPIC='src-sqlserver-jdbc-sampledb-custumer-avro', VALUE_FORMAT='AVRO', KEY_FORMAT='AVRO');
 CREATE OR REPLACE STREAM ksql_stream_sampledb_salesorderheader_avro WITH (KAFKA_TOPIC='src-sqlserver-jdbc-sampledb-salesorderheader-avro', VALUE_FORMAT='AVRO', KEY_FORMAT='AVRO');
-select * from ksql_stream_sampledb_customer_avro emit changes limit 100;
-select * from ksql_stream_sampledb_salesorderheader_avro emit changes limit 10;
+select * from ksql_stream_sampledb_customer_avro emit changes limit 1;
+select * from ksql_stream_sampledb_salesorderheader_avro emit changes limit 1;
 SELECT SalesPerson, count(SalesPerson) as "number_clients" from ksql_stream_sampledb_customer_avro WINDOW SESSION (5 SECONDS) group by SalesPerson EMIT CHANGES;
 
+CREATE OR REPLACE STREAM output_ksql_stream_sampledb_customerdates_avro
+WITH (KAFKA_TOPIC='output_ksql_stream_sampledb_customerdates_avro',PARTITIONS=6, VALUE_FORMAT='AVRO')
+AS
+SELECT
+order.CUSTOMERID,
+order.PURCHASEORDERNUMBER,
+order.ORDERDATE,
+order.SHIPDATE,
+order.DUEDATE,
+CONCAT_WS(' ',CAST(client.FIRSTNAME AS VARCHAR),CAST(client.MIDDLENAME AS VARCHAR),CAST(client.LASTNAME AS VARCHAR)) AS CUSTOMER,
+client.EMAILADDRESS,
+TIMESTAMPTOSTRING(order.ROWTIME,'yyyy-MM-dd HH:mm:ss', 'America/Sao_Paulo') AS "processing_time"
+FROM ksql_stream_sampledb_salesorderheader_avro AS order
+INNER JOIN ksql_stream_sampledb_customer_avro AS client WITHIN 36500 DAYS
+ON order.CUSTOMERID = client.CUSTOMERID
+EMIT CHANGES;
+
+SELECT * FROM output_ksql_stream_sampledb_customerdates_avro emit changes limit 5;
 
 
 
