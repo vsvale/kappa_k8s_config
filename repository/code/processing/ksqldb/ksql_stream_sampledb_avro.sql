8ceffcfafb8017d6e2c6208e77a3e759f3994b73
dataeng <viniciusdvale@gmail.com>
2022-11-03 09:50:49 -0300

    gateway deployment

--- repository/code/processing/ksqldb/ksql_stream_sampledb_avro.sql
+++ repository/code/processing/ksqldb/ksql_stream_sampledb_avro.sql
@@ -3,6 +3,7 @@
 -- kubectl exec $KSQLDB -n processing -ti -- bash ksql
 SET 'auto.offset.reset' = 'earliest';
 SHOW TOPICS;
+SHOW STREAMS;
 CREATE OR REPLACE STREAM ksql_stream_sampledb_customer_avro WITH (KAFKA_TOPIC='src-sqlserver-jdbc-sampledb-custumer-avro', VALUE_FORMAT='AVRO', KEY_FORMAT='AVRO');
 CREATE OR REPLACE STREAM ksql_stream_sampledb_salesorderheader_avro WITH (KAFKA_TOPIC='src-sqlserver-jdbc-sampledb-salesorderheader-avro', VALUE_FORMAT='AVRO', KEY_FORMAT='AVRO');
 select * from ksql_stream_sampledb_customer_avro emit changes limit 1;
